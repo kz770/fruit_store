fb96955af6c229ce748e67323041dfcbdcebb115
유현진 <uhyeonjin0619@gmail.com>
2024-09-06 21:21:29 +0900

    first commit

---
+++ src/main/java/com/example/fruit_store/FruitStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.fruit_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitStoreApplication.class, args);
+    }
+
+}
