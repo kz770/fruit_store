8aef81e9c6537aee73919aae9757dd63d4949b13
유현진 <uhyeonjin0619@gmail.com>
2024-09-10 21:24:25 +0900

    Cart 메서드 추가

--- fruit_back/src/main/java/com/example/fruit_store/dao/CartDAO.java
+++ fruit_back/src/main/java/com/example/fruit_store/dao/CartDAO.java
@@ -4,6 +4,16 @@ import com.example.fruit_store.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartDAO extends JpaRepository<Cart, Integer> {
+
+    // select * from cart where memberid=?
+    // 멤버의 장바구니 정보를 가져온다
+    public List<Cart> findByMember_Id(String id);
+
+    // select * from cart where memeberid=? and goodsno=?
+    // 멤버의 장바구니 안에 있는 특정 상품 레코드를 조회하여 가져온다.
+    public Cart findByMember_idAndGoods_no(String id, int gno);
 }
--- fruit_back/src/main/java/com/example/fruit_store/service/CartService.java
+++ fruit_back/src/main/java/com/example/fruit_store/service/CartService.java
@@ -1,7 +1,33 @@
 package com.example.fruit_store.service;
 
+import com.example.fruit_store.dao.CartDAO;
+import com.example.fruit_store.entity.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartService {
+    @Autowired
+    private CartDAO cartDAO;
+
+    public List<Cart> cartList(String id) {
+        return cartDAO.findByMember_Id(id);
+    }   //멤버의 장바구니 정보 가져오기
+
+    public void insert(Cart c) {
+        c.setQty(1);    // 일단 수량을 한개로 정했지만
+                        // TODO : 추후 특정한 개수를 더하는 로직으로 수정
+        String uid = c.getMember().getId();
+        int gno = c.getGoods().getNo();
+        Cart old = cartDAO.findByMember_idAndGoods_no(uid, gno);
+        if (old != null) {
+            c.setQty(old.getQty()+1);
+            c.setNo(old.getNo()+1);
+        }
+        cartDAO.save(c);
+    }
+
+
 }
